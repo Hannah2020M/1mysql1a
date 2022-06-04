03bc8eec90e201f86c47b6bc5c5a73c1dfb29465
Hannah2020M <coder2020aug@gmail.com>
2022-06-04 18:17:58 -0500

    commit on 2022fjun4Sat

---
+++ java2mysql/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is archive of MySQL stuff I learnt in 2022JuneLC");
+    }
+}
